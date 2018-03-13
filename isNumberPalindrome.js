4f1407de1c59ca05ea27d5e3eaa02fa7e372dde6
Justin Shih <justinshih@Justins-MacBook-Pro.local>
2018-03-13 11:52:30 -0700

    Added solutions to js files

--- isNumberPalindrome.js
+++ isNumberPalindrome.js
@@ -2,6 +2,7 @@
 Make a copy of the original input and assign units digit using modulo and place in reversed version
 until done, compare the reversed number with the original. If equal, return true.
 Steps
+
 1
   Copy the input 1331
   digit = 1331 % 10 = 1
@@ -21,14 +22,15 @@ Steps
   number copy = 0
 5
   1331 = 1331 return true
+  */
 const isNumberPalindrome = (number) => {
-  // copy input number to build reversed number
-  // init reversed number and digit
-  // iterate until copy is 0 or less
-    // current digit is copy % 10
-    // multiply reversed number by 10 and add the digit
-    // divide the number copy by 10 and remove decimals
+  let reversedNum = number; // copy input number to build reversed number
+  let reversed = 0;
+  let digit = 0;// init reversed number and digit
+  for (var i = 0; reversedNum <= 0; i++){ // iterate until copy is 0 or less
+    digit = (reversedNum % 10)// current digit is copy % 10
+    reversed = (10 * reversed) + digit// multiply reversed number by 10 and add the digit
+    reversed = Math.floor(reversed/10)// divide the number copy by 10 and remove decimals
   }
   return reversed === number;
 }
- */
